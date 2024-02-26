e7045c998ec981ff640c019a83225e2db8beddaf
gp21 <galkamoroz@rambler.ru>
2024-02-26 17:02:27 +0300

    260224

--- games.py
+++ games.py
@@ -4,46 +4,47 @@ import random
 
 # p - экран, h1-воздушный кикорик
 n=6
-nf=3
+nf=4
 pygame.init() #страртует пайгейм
 p = pygame.display.set_mode((2000,900)) #экранчик
 h1=pygame.image.load("ппр.png") #скачать героя 1
 h1.set_colorkey((255,255,255)) #установить прозрачность
 e=h1.get_rect(topleft=(10,300)) #перевести в квадрат
 p.blit(h1,(e.x,e.y))# нарисовать
-
+counter=0
 #Делаем сов
+
+
 f1=pygame.image.load("сова.svg")
-s_sq=[0]*nf
+s_sq=[]
 for f in range (nf):
- s_sq[f] = f1.get_rect(topleft=(random.randrange(-600,0),random.randrange(10,1990)))  # перевести в квадрат
+ s_sq.append(f1.get_rect(topleft=(random.randrange(100,1500),random.randrange(-600,0))))  # перевести в квадрат
+
  p.blit(f1, (s_sq[f].x, s_sq[f].y))  # нарисовать
-#r=pygame.Surface((10,10))
 pygame.draw.circle(p,(255,255,0),(100,100),15)
-#p.blit(r,(2,2))
-pygame.display.update()# обновить экран
+pygame.display.update()# обновить экранaq
 x=[]
 y=[]
+w=[0]*n
 for r in range (n):
  y.append(random.randrange(-600,0))
  x.append(random.randrange(10,1990))
 while True:
-#переместить
 
 #Отрисовка
  p.fill((250, 250, 210))#залить
- for r in range (n):
-  pygame.draw.circle(p, (255, 255, 0), (x[r], y[r]), 15)
- p.blit(h1, (e.x, e.y))
  for f in range(nf):
   p.blit(f1, (s_sq[f].x, s_sq[f].y))
+ for r in range (n):
+  w[r]=pygame.draw.circle(p, (255, 255, 0), (x[r], y[r]), 15)
+ p.blit(h1, (e.x, e.y))
+
  pygame.display.update()
 #Выход из игры
 
  q=pygame.event.get()
  for i in q:
   if i.type==pygame.QUIT or i.type==pygame.KEYDOWN and i.key == pygame.K_ESCAPE:
-   print(i)
    sys.exit()
 
 #Двигаем героя
@@ -57,15 +58,17 @@ while True:
  if v[pygame.K_a]:
   e.move_ip(-15, 0)
 #Двигаем сов
-  for f in range(nf):
-   if  s_sq[f].x > 1650:
-    s_sq[f].x = 1650
-   if  s_sq[f].x < 200:
-    s_sq[f].x = 200
-   s_sq[f].move_ip(0, 1)
+ for f in range(nf):
+   if  s_sq[f].y > 900:
+    s_sq[f].y = 0
+    s_sq[f].x =random.randrange(30, 1985)
+   s_sq[f].move_ip(0, 5)
 
-
-
+#Убиваемся об сов
+ for f in range(nf):
+  t=e.colliderect(s_sq[f])
+  if t==True:
+   sys.exit()
   # if i.type == pygame.KEYDOWN and i.key == pygame.K_a:and i.key == pygame.K_ESCAPE
   #  e.move_ip(-11, 0)
   # if i.type == pygame.KEYDOWN and i.key == pygame.K_d:
@@ -77,4 +80,13 @@ while True:
    y[r]=0
    x[r] = random.randrange(10, 1990)
  pygame.time.delay(10)
+#ловим шарики
+ for o in range(n):
+  t = e.colliderect(w[o])
+  if t == True:
+   counter+=1
+   print(counter)
+   y[r] = 0
+   x[r] = random.randrange(10, 1990)
+
 
