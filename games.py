3e8c211f29ede178e5eab558ad3aa09a9d65b9ef
gp21 <galkamoroz@rambler.ru>
2024-03-11 16:46:57 +0300

    110324

--- games.py
+++ games.py
@@ -1,19 +1,24 @@
 import pygame
 import sys
 import random
-
+def Gameover():
+ p.fill((250, 250, 210))
+ sh1= pygame.font.Font(None, 60)
+ mo1 = sh1.render("Game over", True, (0, 0, 0))
+ p.blit(mo1, (290, 100))
+ pygame.display.update()
 # p - экран, h1-воздушный кикорик
-n=2
-nf=3
+n=6
+nf=5
 pygame.init() #страртует пайгейм
-p = pygame.display.set_mode((2000,900)) #экранчик
+p = pygame.display.set_mode((1920,900)) #экранчик
 h1=pygame.image.load("ппр.png") #скачать героя 1
 h1.set_colorkey((255,255,255)) #установить прозрачность
-e=h1.get_rect(topleft=(10,300)) #перевести в квадрат
+e=h1.get_rect(topleft=(900,300)) #перевести в квадрат
 p.blit(h1,(e.x,e.y))# нарисовать
 counter=0
 #Делаем сов
-sh=pygame.font.Font(None,20)
+sh=pygame.font.Font(None,35)
 
 f1=pygame.image.load("сова.svg")
 s_sq=[]
@@ -38,9 +43,9 @@ while True:
  for r in range (n):
   w[r]=pygame.draw.circle(p, (255, 255, 0), (x[r], y[r]), 15)
  p.blit(h1, (e.x, e.y))
+ mo=sh.render(str(counter),True,(0,0,0))
+ p.blit(mo, (290, 100))
  pygame.display.update()
- mo=sh.render(str(counter),True,(3,5,2))
- p.blit(mo, (1500, 500))
 #Выход из игры
 
  q=pygame.event.get()
@@ -69,7 +74,9 @@ while True:
  for f in range(nf):
   t=e.colliderect(s_sq[f])
   if t==True:
-   sys.exit()
+   #sys.exit
+   Gameover()
+
   # if i.type == pygame.KEYDOWN and i.key == pygame.K_a:and i.key == pygame.K_ESCAPE
   #  e.move_ip(-11, 0)
   # if i.type == pygame.KEYDOWN and i.key == pygame.K_d:
@@ -90,3 +97,5 @@ while True:
    counter+=1
    y[o] = 0
    x[o] = random.randrange(10, 1990)
+
+
